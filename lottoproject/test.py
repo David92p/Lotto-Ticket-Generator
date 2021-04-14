2aaac662f701c9c28b390387a744339b8c4c5350
David92p <dave.panetta92@gmail.com>
2021-04-14 15:22:57 +0200

    ADD LP1

--- lottoproject/test.py
+++ lottoproject/test.py
@@ -1,43 +1,28 @@
 from ticket import Ticket
-from header import Header
 
 import argparse
 
 
 def start(n):
-    for i in range(n):
-        ticket = Ticket()
+    tickets = dict()
+    for key in range(n):
+        tickets[key] = Ticket()
+
+    count_ticket = 1
+    for key, values in tickets.items():
+        print(f"Ticket Number {count_ticket}")
+        Ticket.define_ticket(values)
         print()
-        Header.horizontal()
-        Header.vertical()
-        Header.horizontal()
-        Ticket.define_ticket(ticket)
-        print()
-    print("Good Luck")
+        count_ticket += 1
+    print()
+    print("---------> Good Luck <---------")
     print()
 
 
+parser = argparse.ArgumentParser(description="Welcome to Lotto Ticket Generator")
+parser.add_argument("n", type=int, help="Enter the number of tickets to be generated", choices=[1,2,3,4,5])
+args = parser.parse_args()
+
+#test
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Lotto Ticket Generator")
-    parser.add_argument("-n", "-number", type=int, help="Enter the number of tickets to be generated", choices=[0, 1, 2, 3, 4, 5])
-    args = parser.parse_args()
-
-    ticket = args.n
-    # print(ticket)
-
-    if ticket is None:
-        print()
-        print("""You can play 1 to 5 tickets. \nPress 0 if you want to exit the program""")
-        print()
-
-        while True:
-            ticket = int(input("Enter the ticket number to play: "))
-            if 0 < ticket <= 5:
-                break
-            elif ticket == 0:
-                quit()
-            else:
-                print("You entered an invalid value")
-
-    start(ticket)
-
+    start(args.n)
